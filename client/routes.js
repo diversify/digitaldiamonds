c043fadeeed98dd93441ff440775451afafc7cb9
Patrik Gothe <patrik.got@gmail.com>
2014-11-23 12:17:25 +0100

    implement support for adding a spotify playlist

--- client/routes.js
+++ client/routes.js
@@ -53,3 +53,34 @@ Router.map(function() {
 
  // });
 });
+
+
+Meteor.spotifyAPI = {
+	createList: function () {
+			if(Meteor.user()){
+			$.ajax({
+				type:'POST',
+				headers: {
+		        	'Authorization': 'Bearer ' + "BQDU6NUwh0wr8zyp0q0s1IkYAWyVtPozooGC4e9gUUwa1k-YK8Vy-o15U4YFIEGBqhX0vZ4OKDDyfJfK30Es535zU_shPmVE6Bzl4g47k3jQyHUwp4SgEhZXqZET7QCtmTcxRnH2arjMoccP5L5t6xXXQ2fjJJCOQ4dsBg1r5xrHfmyY8_yQYpoH",
+					'Content-Type': 'application/json',
+					'Accept': 'application/json'
+		    	},
+		    	data: '{"name":"'+Meteor.user().services.twitter.screenName+'","public": "true"}',
+				url: 'https://api.spotify.com/v1/users/charlottewohlecke/playlists',
+				success: function (response) {
+					alert('never happens');
+					console.log(response);
+
+					Meteor.users.update(
+						{
+							_id:Meteor.user()._id
+						}, {
+							$set:{'profile.playlist': response.external_urls.spotify}
+
+						}
+					);
+				}
+			});
+		}
+	}
+};
