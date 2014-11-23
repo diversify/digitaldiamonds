93cf09b856832810c23fc1f106efe269d5238f64
Patrik Gothe <patrik.got@gmail.com>
2014-11-23 09:49:04 +0100

    spotify auth

--- client/routes.js
+++ client/routes.js
@@ -17,5 +17,29 @@ Router.map(function() {
 		}
 	});
 
+	this.route('auth', {
+		path: '/auth',
+		action: function () {
+			var hash = {};
+			location.hash.replace(/^#\/?/, '').split('&').forEach(function(kv) {
+				var spl = kv.indexOf('=');
+				if (spl != -1) {
+					hash[kv.substring(0, spl)] = decodeURIComponent(kv.substring(spl+1));
+				}
+			});
+
+			if (hash.access_token) {
+				window.opener.postMessage(JSON.stringify({
+					type:'access_token',
+					access_token: hash.access_token,
+					expires_in: hash.expires_in || 0
+				}), '*');
+
+				window.close();
+			}
+		}
+	});
+
+
 
  });
